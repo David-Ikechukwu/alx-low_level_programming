1ae1d22e6d88476ef5047ed7074003742fe43166
David-Ikechukwu <davidikechukwu1999@gmail.com>
2023-02-16 23:28:56 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by new line.
  * Return: Always 0.
  */
 
+
 int main(void)
+
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
